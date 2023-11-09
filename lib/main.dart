03403dbd0de439ee19e4dc8e83def99bedae46c0
minchodang <minsu910725@gmail.com>
2023-11-09 13:27:16 +0900

    Feat: login form 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,70 +3,92 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  int counter = 0;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('You have pushed the button this many times:'),
-                Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FloatingActionButton(
-                      child: Icon(Icons.add),
-                      onPressed: () {
-                        setState(() {
-                          counter++;
-                          print("$counter");
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    FloatingActionButton(
-                      child: Icon(Icons.remove),
-                      onPressed: () {
-                        setState(() {
-                          counter--;
-                          print("$counter");
-                        });
-                      },
-                    )
-                  ],
-                )
-              ],
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              setState(() {
-                counter++;
-                print("$counter");
-              });
-            },
-          ),
-        ));
+            Form(
+                child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                          labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15.0,
+                      )),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration:
+                                InputDecoration(labelText: 'Enter "dice"'),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            decoration:
+                                InputDecoration(labelText: 'Enter "password"'),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          ButtonTheme(
+                              minWidth: 100,
+                              height: 100,
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.amber,
+                                      textStyle: const TextStyle(
+                                        color: Colors.amber,
+                                        fontSize: 20,
+                                      )),
+                                  onPressed: () {},
+                                  child: const Icon(Icons.arrow_forward)))
+                        ],
+                      ),
+                    )))
+          ],
+        ),
+      ),
+    );
   }
 }
