9bc5ff1f6ce83c0b097aec8616bd1e90bcca5117
minchodang <minsu910725@gmail.com>
2023-11-21 21:54:15 +0900

    Feat: 주사위 페이지 추가

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,37 +1,70 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import "package:fluttertoast/fluttertoast.dart";
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.redAccent,
       appBar: AppBar(
+        backgroundColor: Colors.redAccent,
         title: Text('Dice'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/b');
-              },
-              child: Text('Go to ScreenB'),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Row(
+              children: [
+                Expanded(child: Image.asset('image/dice$leftDice.png')),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(child: Image.asset('image/dice$rightDice.png')),
+              ],
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent),
               onPressed: () {
-                Navigator.pushNamed(context, '/c');
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                  rightDice = Random().nextInt(6) + 1;
+                });
+                showToast('Left dice: {$leftDice}, Right dice: {$rightDice}');
               },
-              child: Text('Go to ScreenC'),
-            )
-          ],
-        ),
-      ),
+              child: Icon(Icons.play_arrow),
+            ),
+          )
+        ],
+      )),
     );
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+    msg: message,
+    textColor: Colors.black,
+    backgroundColor: Colors.white,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+  );
 }
