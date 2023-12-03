6dac63498ba1f408510798acce5465fa0e193a24
minchodang <minsu910725@gmail.com>
2023-12-03 09:10:38 +0900

    Feat: 날씨앱 초기셋팅, api 및 관련 로직 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/Loading.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,106 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Future',
+      title: 'Weather app',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Home(),
+      home: Loading(),
     );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  String result = 'no data found';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Future test'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  futureTest();
-                },
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Future test',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                result,
-                style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
-              ),
-              Divider(
-                height: 20.0,
-                thickness: 2.0,
-              ),
-              FutureBuilder(
-                future: myFuture(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      // Use the data if it's not null
-                      return Text(
-                        snapshot.data!,
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.blue,
-                        ),
-                      );
-                    } else {
-                      // Handle the case where data is null
-                      return Text(
-                        "No data available",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.red,
-                        ),
-                      );
-                    }
-                  }
-                  return CircularProgressIndicator();
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<void> futureTest() async {
-    await Future.delayed(Duration(seconds: 3)).then((value) {
-      print('Here comes second');
-
-      setState(() {
-        this.result = 'The data is fetched';
-        print(result);
-        print('Here comes third');
-      });
-    });
-    print('Here comes first');
-    print('Here is the last one');
-  }
-
-  Future<String> myFuture() async {
-    await Future.delayed(Duration(seconds: 2));
-    return 'another Future completed';
   }
 }
