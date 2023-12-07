f427c648bc3c83afa7ec67d652d818b0ce141651
minchodang <minsu910725@gmail.com>
2023-12-07 23:48:04 +0900

    Feat: data-fetching-test

--- lib/screens/Loading.dart
+++ lib/screens/Loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/dice.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,13 +11,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   var currentPosition;
   Function getLocation = () async {
     LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    showToast(position.toString());
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      showToast(position.toString());
+    } catch (e) {
+      print('위치정보 실패입니다.');
+    }
   };
+
+  void fetchData() async {
+    Response response = await get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    print(response);
+  }
+
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style = ElevatedButton.styleFrom(
@@ -25,9 +45,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(
