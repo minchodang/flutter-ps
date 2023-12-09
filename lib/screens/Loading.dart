c8849d588b75937bdacbbd68dec3b4f91e24f286
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-12-09 21:02:31 +0900

    Feat: data-fetching-test (#17)
    * Feat: data-fetching-test
    
    * Feat: json-parsing 로직

--- lib/screens/Loading.dart
+++ lib/screens/Loading.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/dice.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,13 +13,41 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   var currentPosition;
   Function getLocation = () async {
     LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    showToast(position.toString());
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      showToast(position.toString());
+    } catch (e) {
+      print('위치정보 실패입니다.');
+    }
   };
+
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      print(wind);
+      var id = jsonDecode(jsonData)['id'];
+      print(id);
+    }
+    // print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style = ElevatedButton.styleFrom(
@@ -25,9 +56,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(
