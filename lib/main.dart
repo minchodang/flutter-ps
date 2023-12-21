1013e1377f2ced89b5912676e15062771d4026a6
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-12-21 13:41:27 +0900

    feat: global-key 관련 예제 추가 (#22)

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/dice.dart';
 import 'package:flutter_application_1/screens/key/KeyLogin.dart';
+import 'package:flutter_application_1/screens/key/MyKey.dart';
+import 'package:flutter_application_1/screens/key/ScaffoldKey.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+final rootScaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather app',
+      title: 'scaffold key',
+      scaffoldMessengerKey: rootScaffoldKey,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: KeyLogin(),
+      home: ScaffoldKey(),
     );
   }
 }
