7343cc6abb85381969ae039c45644f1cf128d812
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-12-21 11:48:45 +0900

    feat: login input key 예제 (#21)

---
+++ lib/screens/key/KeyLogin.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/dice.dart';
+
+class KeyLogin extends StatefulWidget {
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<KeyLogin> {
+  bool emailField = true;
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
+      ),
+      body: Builder(
+        builder: (context) {
+          return GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  Center(
+                    child: Image(
+                      image: AssetImage('image/other/chef.gif'),
+                      width: 170.0,
+                      height: 190.0,
+                    ),
+                  ),
+                  Form(
+                      child: Theme(
+                          data: ThemeData(
+                              primaryColor: Colors.teal,
+                              inputDecorationTheme: InputDecorationTheme(
+                                  labelStyle: TextStyle(
+                                      color: Colors.teal, fontSize: 15.0))),
+                          child: Container(
+                            padding: EdgeInsets.all(40.0),
+                            child: Column(
+                              children: <Widget>[
+                                if (emailField)
+                                  TextField(
+                                    key: ValueKey(1),
+                                    // controller: controller,
+                                    decoration: InputDecoration(
+                                        labelText: 'Enter "image"'),
+                                    keyboardType: TextInputType.emailAddress,
+                                  ),
+                                TextField(
+                                  key: ValueKey(2),
+                                  // controller: controller2,
+                                  decoration: InputDecoration(
+                                      labelText: 'Enter Password'),
+                                  keyboardType: TextInputType.text,
+                                  obscureText: false,
+                                ),
+                                SizedBox(
+                                  height: 40.0,
+                                ),
+                                ButtonTheme(
+                                    minWidth: 100.0,
+                                    height: 50.0,
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.cyan,
+                                          minimumSize: Size(150, 50),
+                                        ),
+                                        child: Icon(
+                                          Icons.visibility_off,
+                                          color: Colors.white,
+                                          size: 35.0,
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            emailField = false;
+                                          });
+                                        })),
+                              ],
+                            ),
+                          )))
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+void showSnackBar(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '로그인 정보를 다시 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar2(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '비밀번호가 일치하지 않습니다',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackBar3(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      'dice의 철자를 확인하세요',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
