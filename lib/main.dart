da0f90cffa3fc85d63bea11e0156be76982a9d00
minchodang <minsu910725@gmail.com>
2024-01-10 20:35:15 +0900

    Feat: provider 관련 예제 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,8 @@ import 'package:flutter_application_1/screens/chat/chat_screen.dart';
 import 'package:flutter_application_1/screens/chat/main_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeDefault();
   runApp(MyApp());
 }
-
-Future<void> initializeDefault() async {
-  FirebaseApp app = await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  print('intiallized default app $app');
-}
-
-final rootScaffoldKey = GlobalKey<ScaffoldMessengerState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -26,18 +15,95 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'weather application',
-      scaffoldMessengerKey: rootScaffoldKey,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ChatScreen();
-          }
-          return LoginSignupScreen();
-        },
+      home: FishOrder(),
+    );
+  }
+}
+
+class FishOrder extends StatelessWidget {
+  const FishOrder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fish Order'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'Fish name',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            High()
+          ],
+        ),
       ),
     );
+  }
+}
+
+class High extends StatelessWidget {
+  const High({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'SpicyA is located at high place',
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        SpicyA()
+      ],
+    );
+  }
+}
+
+class SpicyA extends StatelessWidget {
+  const SpicyA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Fish number',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Fish size',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Middle()
+      ],
+    );
+  }
+}
+
+class Middle extends StatelessWidget {
+  const Middle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
