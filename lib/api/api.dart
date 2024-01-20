1014725dfe9b77558059f552c66aedcd8dca05ec
minchodang <minsu910725@gmail.com>
2024-01-20 11:29:45 +0900

    Feat: mysql-api signup연동

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,3 +1,7 @@
 class API {
   static const hostConnect = "http://192.168.0.71/api_new_members";
+  static const hostConnectUser = "$hostConnect/user";
+
+  static const signup = "$hostConnect/user/signup.php";
+  static const validateEmail = "$hostConnect/user/validate_email.php";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/fire_store/login.dart';
+import 'package:flutter_application_1/authentication/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
